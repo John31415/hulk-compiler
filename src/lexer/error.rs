30da4b7efb21570834aed6ccb91f922df89366e9
John31415 <johnmauris04@gmail.com>
2026-05-11 13:13:37 -0400

    feat(lexer): add LexErrorKind and LexError

---
+++ src/lexer/error.rs
@@ -0,0 +1,32 @@
+use super::Span;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LexErrorKind {
+    UnexpectedCharacter,
+    UnclosedString,
+    Unknown,
+}
+
+impl LexErrorKind {
+    pub fn from_slice(slice: &str) -> Self {
+        if slice.is_empty() {
+            return Self::Unknown;
+        }
+        match slice {
+            s if s.starts_with('"') && (s.len() == 1 || !s.ends_with('"')) => Self::UnclosedString,
+            _ => Self::UnexpectedCharacter
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct LexError {
+    pub kind: LexErrorKind,
+    pub span: Span,
+}
+
+impl LexError {
+    pub fn new(kind: LexErrorKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
