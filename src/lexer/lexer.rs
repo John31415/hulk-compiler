5c5e03ce01d66d8e48e1dce54f7f6da5419b2a25
John31415 <johnmauris04@gmail.com>
2026-05-11 13:14:46 -0400

    feat(lexer): add Lexer, based on logos, implements tokenizer

---
+++ src/lexer/lexer.rs
@@ -0,0 +1,53 @@
+use logos::Logos;
+
+use crate::lexer::{LexError, LexErrorKind, Span, Token, TokenKind};
+
+pub struct Lexer<'a> {
+    inner: logos::Lexer<'a, TokenKind>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            inner: TokenKind::lexer(input),
+        }
+    }
+
+    pub fn next_token(&mut self) -> Result<Token, LexError> {
+        match self.inner.next() {
+            Some(result) => {
+                let kind = result.unwrap_or(TokenKind::Error);
+
+                let span = Span::from_range(self.inner.span());
+                if kind == TokenKind::Error {
+                    Err(LexError::new(
+                        LexErrorKind::from_slice(self.inner.slice()),
+                        span,
+                    ))
+                } else {
+                    Ok(Token { kind, span })
+                }
+            }
+            None => {
+                let span = Span::from_range(self.inner.span());
+                Ok(Token {
+                    kind: TokenKind::EOF,
+                    span,
+                })
+            }
+        }
+    }
+
+    pub fn tokenize(&mut self) -> Result<Vec<Token>, LexError> {
+        let mut tokens = Vec::new();
+        loop {
+            let token = self.next_token()?;
+            let is_eof = token.kind == TokenKind::EOF;
+            tokens.push(token);
+            if is_eof {
+                break;
+            }
+        }
+        Ok(tokens)
+    }
+}
