efbaa880b1cc67233c66812e5cbe63eb27f9dea5
John31415 <johnmauris04@gmail.com>
2026-05-21 04:58:47 -0400

    feat(src): implement full abstract syntax tree structure

---
+++ src/ast.rs
@@ -0,0 +1,179 @@
+use crate::lexer::Span;
+use serde::Serialize;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Spanned<T> {
+    pub node: T,
+    pub span: Span,
+}
+
+impl<T> Spanned<T> {
+    pub fn new(kind: T, span: Span) -> Self {
+        Self { node: kind, span }
+    }
+}
+
+pub type Program = Spanned<ProgramKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct ProgramKind {
+    pub delcs: Option<Vec<Decl>>,
+    pub body: Expr,
+}
+
+pub type Decl = Spanned<DeclKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum DeclKind {
+    Function {
+        name: String,
+        params: Vec<(String, Option<String>)>,
+        return_type: Option<String>,
+        body: Expr,
+    },
+    Type {
+        name: String,
+        params: Option<Vec<(String, Option<String>)>>,
+        parent: Option<InheritInfo>,
+        features: Vec<TypeFeatures>,
+    },
+}
+
+pub type Expr = Spanned<ExprKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum ExprKind {
+    Literal(Literal),
+    Variable(String),
+    New {
+        type_name: String,
+        args: Vec<Expr>,
+    },
+    Block(Vec<Expr>),
+    Call {
+        name: String,
+        args: Vec<Expr>,
+    },
+    PropertyAccess {
+        obj: Box<Expr>,
+        property: String,
+    },
+    MethodCall {
+        obj: Box<Expr>,
+        method: String,
+        args: Vec<Expr>,
+    },
+    Unary {
+        op: UnaryOp,
+        expr: Box<Expr>,
+    },
+    Binary {
+        left_expr: Box<Expr>,
+        op: BinaryOp,
+        right_expr: Box<Expr>,
+    },
+    Is {
+        expr: Box<Expr>,
+        type_name: String,
+    },
+    As {
+        expr: Box<Expr>,
+        type_name: String,
+    },
+    Let {
+        name: String,
+        type_name: Option<String>,
+        value: Box<Expr>,
+        body: Box<Expr>,
+    },
+    If {
+        condition: Box<Expr>,
+        then_branch: Box<Expr>,
+        else_branch: Option<Box<Expr>>,
+    },
+    While {
+        condition: Box<Expr>,
+        body: Box<Expr>,
+    },
+    For {
+        var: String,
+        iterable: Box<Expr>,
+        body: Box<Expr>,
+    },
+    Assign {
+        target: Box<Expr>,
+        value: Box<Expr>,
+    },
+}
+
+pub type TypeFeatures = Spanned<TypeFeaturesKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum TypeFeaturesKind {
+    Attribute {
+        name: String,
+        type_name: Option<String>,
+        default: Option<Expr>,
+    },
+    Method {
+        name: String,
+        params: Vec<(String, Option<String>)>,
+        return_type: Option<String>,
+        body: Expr,
+    },
+}
+
+pub type InheritInfo = Spanned<InheritInfoKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct InheritInfoKind {
+    pub parent_name: String,
+    pub args: Option<Vec<Expr>>,
+}
+
+pub type Literal = Spanned<LiteralKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum LiteralKind {
+    Number(f64),
+    String(String),
+    Bool(bool),
+}
+
+pub type UnaryOp = Spanned<UnaryOpKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum UnaryOpKind {
+    Not,
+    Neg,
+}
+
+pub type BinaryOp = Spanned<BinaryOpKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub enum BinaryOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    Concat,
+    ConcatSpace,
+    Less,
+    Greater,
+    LessEqual,
+    GreaterEqual,
+    DoubleEqual,
+    NotEqual,
+    And,
+    Or,
+}
+
+pub type VarDef = Spanned<VarDefKind>;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct VarDefKind {
+    pub name: String,
+    pub type_name: Option<String>,
+    pub value: Expr,
+}
