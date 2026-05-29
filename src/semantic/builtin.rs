ef4a5e76498c5a3b6b48af24a361c5bbb8525c8b
John31415 <johnmauris04@gmail.com>
2026-05-28 23:03:44 -0400

    feat(semantic): add built-in math functions and constants

---
+++ src/semantic/builtin.rs
@@ -0,0 +1,75 @@
+use super::{
+    context::SemanticContext,
+    symbols::{Symbol, SymbolKind, SymbolType},
+};
+use crate::lexer::span::Span;
+
+pub fn install_builtins(ctx: &mut SemanticContext) {
+    let number = ctx.types.resolve("Number").unwrap();
+    ctx.declare(Symbol {
+        name: "sqrt".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![number],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "sin".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![number],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "cos".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![number],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "exp".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![number],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "log".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![number, number],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "rand".to_string(),
+        kind: SymbolKind::Function,
+        ty: SymbolType::Function {
+            params: vec![],
+            ret: number,
+        },
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "PI".to_string(),
+        kind: SymbolKind::Variable,
+        ty: SymbolType::Variable(number),
+        span: Span::new(0, 0),
+    });
+    ctx.declare(Symbol {
+        name: "E".to_string(),
+        kind: SymbolKind::Variable,
+        ty: SymbolType::Variable(number),
+        span: Span::new(0, 0),
+    });
+}
