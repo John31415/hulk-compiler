c008caf5158e2046c4a2131ba4714c1b5ab22ab3
John31415 <johnmauris04@gmail.com>
2026-05-30 01:52:37 -0400

    feat(semantic): add mod to export public features

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -2,8 +2,10 @@ pub mod analyzer;
 pub mod builtin;
 pub mod context;
 pub mod decl;
+pub mod error;
 pub mod expr;
 pub mod symbols;
+pub mod test_utils;
 pub mod types;
 
 pub use analyzer::SemanticAnalyzer;
