a13ff5064505deca2dcb5fec17613c434f629b46
John31415 <johnmauris04@gmail.com>
2026-05-30 01:49:29 -0400

    tests(semantic): add test utils

---
+++ src/semantic/test_utils.rs
@@ -0,0 +1,12 @@
+use chumsky::Parser;
+
+use crate::{ast::Program, lexer::Lexer, parser::program::program_parser};
+
+pub fn parse_program(source: &str) -> Program {
+    let tokens = Lexer::new(&source).tokenize().expect("Lexer error");
+    let program = program_parser()
+        .parse(&tokens.as_slice())
+        .into_result()
+        .expect("Parser error");
+    program
+}
