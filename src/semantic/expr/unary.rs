16475c92e8e94477e6ea8fa9844f4d75656f2086
John31415 <johnmauris04@gmail.com>
2026-05-31 00:54:20 -0400

    test(semantic): add tests for unary expressions semantic checking

--- src/semantic/expr/unary.rs
+++ src/semantic/expr/unary.rs
@@ -41,3 +41,39 @@ impl SemanticAnalyzer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::semantic::{SemanticAnalyzer, error::SemanticErrorKind, test_utils::parse_program};
+
+    #[test]
+    fn semantic_unit_test_unary() {
+        let source = r#"
+{
+    -true;
+    !1;
+}
+        "#;
+        let program = parse_program(source);
+        let mut analyzer = SemanticAnalyzer::new();
+        analyzer.analyze_program(
+            program.node.decls.as_deref().unwrap_or(&[]),
+            Some(&program.node.body),
+        );
+        assert_eq!(analyzer.diagnostics.len(), 2);
+        assert_eq!(
+            analyzer.diagnostics[0].kind,
+            SemanticErrorKind::InvalidUnaryOperation {
+                operator: "-".to_string(),
+                operand: "Boolean".to_string()
+            }
+        );
+        assert_eq!(
+            analyzer.diagnostics[1].kind,
+            SemanticErrorKind::InvalidUnaryOperation {
+                operator: "!".to_string(),
+                operand: "Number".to_string()
+            }
+        );
+    }
+}
